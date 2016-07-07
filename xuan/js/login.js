eb6a4cbfb23c5e483ba5b77a413caf890b40e376
x790144106 <x790144106@163.com>
2016-07-07 20:48:26 +0800

    8

--- xuan/js/login.js
+++ xuan/js/login.js
@@ -1,9 +1,9 @@
 $(function(){
     var his=window.location.search.split("=")[1];
     $(".register").click(function(){
-        window.location.href="registered.html?his"+his;
+        window.location.href="registered.html?his="+his;
     });
     $(".forget_password").click(function(){
-        window.location.href="password.html?his"+his;
+        window.location.href="password.html?his="+his;
     });
-);}
+});
