f041ee1233434493d834c25aea2b3d92c0ab310e
x790144106 <x790144106@163.com>
2016-07-08 14:55:42 +0800

    9.11111

--- xuan/js/login.js
+++ xuan/js/login.js
@@ -1,9 +1,38 @@
 $(function(){
-    var his=window.location.search.split("=")[1];
+    $("#login").click(function(event){
+        event.stopPropagation();
+        var User=$("#user").val();
+        var Pass=$("#pass").val();
+        // var Pass=her_md5($("#pass").val());
+        if(User=="123456789"&&Pass=="111111"){
+            window.location.href = "index.html";
+        }else{
+            alert("用户名未注册或密码错误");  
+        };
+        
+        // if (User!=""&&Pass!="") {
+        //     $.ajax({
+        //         url:"#",
+        //         type:"POST",
+        //         data:{phone:User,password:Pass},
+        //         datatype:json,
+        //         contenttype:"application/json;charset=UTF-8",
+        //         success:function(data){
+        //             if (data.code==101) {
+        //                 alert("用户名未注册或密码错误");
+        //             }else{
+        //                 console.log(data.message);
+        //                 window.location.href = "index.html";
+        //             };  
+        //         }
+        //     });
+        // };
+
+    });
     $(".register").click(function(){
-        window.location.href="registered.html?his="+his;
+        window.location.href="registered.html";
     });
     $(".forget_password").click(function(){
-        window.location.href="password.html?his="+his;
+        window.location.href="password.html";
     });
 });
