d7c6a8cc3eed649a35875ad434a52a55185c6cf3
x790144106 <x790144106@163.com>
2016-07-27 17:59:00 +0800

    10.19

--- node/ab.php
+++ node/ab.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
-//header("Access-Control-Allow-Origin:*");  /*星号表示所有的域都可以接受，*/ 
-//header("Access-Control-Allow-Methods:GET,POST"); 
+header("Access-Control-Allow-Origin:*");  /*星号表示所有的域都可以接受，*/ 
+header("Access-Control-Allow-Methods:GET,POST"); 
 
 $username = $_POST['username'];
 $age = $_POST['age'];
